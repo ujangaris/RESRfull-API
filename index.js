5defe59b6624b89d015534c91d1d6cb3a03c8f63
ujangaja <ujangaja@gmail.com>
2021-10-17 05:56:54 +0700

    connnect to database mongodb

--- index.js
+++ index.js
@@ -1,5 +1,17 @@
+//import express
 import express from "express";
+//import mongoose
+import mongoose from "mongoose";
 const app = express();
+
+// connect ke database mongoDB
+mongoose.connect("mongodb://localhost:27017/resful_db", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+const db = mongoose.connection;
+db.on("error", (error) => console.error(error));
+db.once("open", () => console.log("Database Connected"));
 
 app.get("/", (req, res) => {
   res.send("Welcome");
