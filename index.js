acf902e8b88fc330bffd1ca62cac7cd9fe93e7b1
ujangaja <ujangaja@gmail.com>
2021-10-16 07:26:45 +0700

    page pertama pada brwoser

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 import express from "express";
-
 const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Welcome");
+});
 
 app.listen("3000", () => console.log("Server running at port: 3000"));
