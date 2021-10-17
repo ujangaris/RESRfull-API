e47d287b3674b8a7c2b5949d7abfa03b8d9c8317
ujangaja <ujangaja@gmail.com>
2021-10-17 09:30:36 +0700

    PATCH data (UPDATE)

--- controllers/productController.js
+++ controllers/productController.js
@@ -21,3 +21,16 @@ export const saveProduct = async (req, res) => {
     res.status(400).json({ message: error.message });
   }
 };
+
+// function Create Product
+export const updateProduct = async (req, res) => {
+  try {
+    const updatedProduct = await Product.updateOne(
+      { _id: req.params.id },
+      { $set: req.body }
+    );
+    res.status(200).json(updatedProduct);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+};
--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,21 @@
 import Express from "express";
-import { getProducts, saveProduct } from "../controllers/productController.js";
+import {
+  getProducts,
+  saveProduct,
+  updateProduct,
+} from "../controllers/productController.js";
 
 const router = Express.Router();
 
 // Route get All Products
 router.get("/", getProducts);
+// Route get single Product
+router.get("/:id");
 // Route CREATE Product
 router.post("/", saveProduct);
+// Route UPDATE Product
+router.patch("/:id", updateProduct);
+// Route DELETE Product
+router.delete("/");
 
 export default router;
