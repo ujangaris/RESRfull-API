86af98028cda970324dccfbe072c93026a04dbcf
ujangaja <ujangaja@gmail.com>
2021-10-17 14:47:07 +0700

    validation untuk single data, update dan delete

--- controllers/productController.js
+++ controllers/productController.js
@@ -12,6 +12,8 @@ export const getProducts = async (req, res) => {
 };
 // function get single Product
 export const getProductsById = async (req, res) => {
+  const cekId = await Product.findById(req.params.id);
+  if (!cekId) return res.status(404).json({ message: "Data tidak ditemukan!" });
   try {
     const product = await Product.findById(req.params.id);
     res.json(product);
@@ -33,6 +35,8 @@ export const saveProduct = async (req, res) => {
 
 // function Update Product
 export const updateProduct = async (req, res) => {
+  const cekId = await Product.findById(req.params.id);
+  if (!cekId) return res.status(404).json({ message: "Data tidak ditemukan!" });
   try {
     const updatedProduct = await Product.updateOne(
       { _id: req.params.id },
@@ -46,10 +50,10 @@ export const updateProduct = async (req, res) => {
 
 // function Delete Product
 export const deleteProduct = async (req, res) => {
+  const cekId = await Product.findById(req.params.id);
+  if (!cekId) return res.status(404).json({ message: "Data tidak ditemukan!" });
   try {
-    const deletedProduct = await Product.deleteOne(
-      { _id: req.params.id }
-    );
+    const deletedProduct = await Product.deleteOne({ _id: req.params.id });
     res.status(200).json(deletedProduct);
   } catch (error) {
     res.status(400).json({ message: error.message });
