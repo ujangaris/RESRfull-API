1b8d793c79218e09df6eddd705f10015e083b4cd
ujangaja <ujangaja@gmail.com>
2021-10-17 15:12:56 +0700

    CORS (cross-Origin-Resource-Sharing)

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ import express from "express";
 import mongoose from "mongoose";
 //import route
 import route from "./routes/index.js";
+//import cors
+import cors from "cors";
+
 const app = express();
 
 // connect ke database mongoDB
@@ -16,6 +19,7 @@ db.on("error", (error) => console.error(error));
 db.once("open", () => console.log("Database Connected"));
 
 // middleware
+app.use(cors());
 app.use(express.json());
 app.use("/product", route);
 
